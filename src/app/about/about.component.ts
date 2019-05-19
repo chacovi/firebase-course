70f455b07114e3615202f07c8f8e5746c986483a
charles.cordova <charles@cordova.me>
2019-05-19 02:52:23 -0500

    firebase snapshotChanges impl

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,23 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
-import * as firebase from 'firebase';
-import 'firebase/firestore';
-import {Course} from '../model/course';
-
-const config = {
-  apiKey: 'AIzaSyAhBHyDk7P3r1wrHQJRtrBUcWYljuA_IIc',
-  authDomain: 'fir-course-17eff.firebaseapp.com',
-  databaseURL: 'https://fir-course-17eff.firebaseio.com',
-  projectId: 'fir-course-17eff',
-  storageBucket: 'fir-course-17eff.appspot.com',
-  messagingSenderId: '857574139960',
-  appId: '1:857574139960:web:fafd234e5ecec445'
-};
-
-firebase.initializeApp(config);
-const db = firebase.firestore();
-const settings = {timestampsInSnapshots: true};
-db.settings(settings);
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'about',
@@ -26,27 +8,23 @@ db.settings(settings);
 })
 export class AboutComponent implements OnInit {
 
-  constructor() {
+  constructor(private  db: AngularFirestore) {
   }
 
   ngOnInit() {
-
-    /*db.doc('courses/3ymtflumj0WVQ9y5coSu')
-      .get()
-      .then(snap => console.log(snap.data()));*/
-
-    db.collection('courses')
-      .get()
-      .then(snaps => {
-        // console.log(snaps);
-        // console.log(snaps.docs.map(snap => snap.data()));
-        const courses: Course[] = snaps.docs.map(snap => {
+    /* this.db.collection('courses')
+       .valueChanges()
+       .subscribe(val => console.log(val));*/
+  /*  this.db.collection('courses')
+      .snapshotChanges()
+      .subscribe(snaps => {
+        const courses: Course[] = snaps.map(snap => {
           return <Course>{
-            id: snap.id,
-            ...snap.data()
+            id: snap.payload.doc.id,
+            ...snap.payload.doc.data()
           };
         });
         console.log(courses);
-      });
+      });*/
   }
 }
